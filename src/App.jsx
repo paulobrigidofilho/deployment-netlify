d8b06bdf56e0df39966e11ba519655c1fabb332a
Paulo Filho <orbishistorymaps@gmail.com>
2024-12-10 20:17:34 +1300

    Done 3

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ function App() {
     <>
       <h1>This will be deployed</h1>
       <h2>This has been added after the Netlify Deploy</h2>
+      <h3>This has been added after the Vercel Deploy</h3>
     </>
   )
 }
